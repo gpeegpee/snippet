672d6e4246ff52a402d7e16fd1669d84effaac12
gpeegpee <gpeegpee@gmail.com>
2015-12-09 13:14:31 +0900

    Update sort_algorithm.cpp

--- sort_algorithm.cpp
+++ sort_algorithm.cpp
@@ -6,12 +6,9 @@
 
 using namespace std;
 
-#define MAX (1024*8)
+#define MAX (1024*8*4)
 int array[MAX+1];
 int org_array[MAX+1];
-
-#define STACK_SIZE (2)
-int stack[STACK_SIZE];
 
 inline void exchange(int data[], int i, int j)
 {
@@ -122,6 +119,41 @@ void merge_sort(int data[], int length, int front, int rear)
 	}
 }
 
+/*
+function select(list, left, right, n)
+{
+    if left = right
+        return left
+    loop
+        pivotIndex := pivot(list, left, right)
+        pivotIndex := partition(list, left, right, pivotIndex)
+        if n = pivotIndex
+            return n
+        else if n < pivotIndex
+            right := pivotIndex - 1
+        else
+            left := pivotIndex + 1
+}
+
+pivot selection
+  function pivot(list, left, right)
+     // for 5 or less elements just get median
+     if right - left < 5:
+         return partition5(list, left, right)
+     // otherwise move the medians of five-element subgroups to the first n/5 positions
+     for i from left to right in steps of 5
+         // get the median of the i'th five-element subgroup
+         subRight := i + 4
+         if subRight > right:
+             subRight := right
+
+         median5 := partition5(list, i, subRight)
+         swap list[median5] and list[left + floor((i - left)/5)]
+
+     // compute the median of the n/5 medians-of-five
+     return select(list, left, left + ceil((right - left) / 5) - 1, left + (right - left)/10)
+*/
+
 int partition(int data[], int front, int rear)
 {
 	int pivot_position = rear;
@@ -141,15 +173,16 @@ int partition(int data[], int front, int rear)
 	return smaller_index;
 }
 
+// max of stack size : 2
 void quick_sort_iterative (int arr[], int front, int rear)
 {
-    //int* stack = (int*)calloc(MAX, sizeof(int));
-
+	int stack[2];
     int top = -1;
 
     stack[ ++top ] = front;
     stack[ ++top ] = rear;
 
+    int max = 0;
     while ( top >= 0 )
     {
         rear = stack[ top-- ];
@@ -169,12 +202,15 @@ void quick_sort_iterative (int arr[], int front, int rear)
             stack[ ++top ] = rear;
         }
 
-        assert(top <= STACK_SIZE);
+        if (top > max)
+        {
+        	max = top;
+        }
+        assert(top + 1 <= 2);
     }
-
-    //free(stack);
 }
 
+// stack overflow can be occurred
 void quick_sort(int data[], int front, int rear)
 {
 	if (front < rear)
